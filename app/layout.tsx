22a2efb7a340ffadf0ea2c372d89a957140e8674
Jeevan <jeevansathyan4u@gmail.com>
2025-08-15 23:16:29 +0530

    feature/ product details added and navbar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ThemeRegistry from "./theme.registry";
+import NavBar from "./NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,11 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <ThemeRegistry options={{ key: "mui" }}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NavBar />
+          {children}
+        </body>
+      </ThemeRegistry>
     </html>
   );
 }
